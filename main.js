4a0850091c6c48497a02139ddd5aacf3ec20e8f7
Shruti Shrivastava <shrutishri2208@gmail.com>
2023-05-05 01:44:26 +0530

    gettingTerms

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ window.onload = onWindowLoad;
 
 let clickBtn = document.getElementById("click-btn");
 let heading = document.getElementById("heading");
+let list = document.getElementById("list");
+
+// clickBtn.addEventListener("click", () => {
+//   let sel = document.querySelectorAll(".trial");
+//   sel.forEach((item) => console.log(item.innerText));
+// });
 
 let searchTerm;
 
@@ -22,12 +28,16 @@ function onWindowLoad() {
             return chrome.scripting.executeScript({
               target: { tabId: activeTabId },
               func: DOMtoString,
-              args: ["title"],
+              args: ["title", "h1", "h2", "h3"],
             });
           })
           .then(function (results) {
-            searchTerm = results[0].result;
-            console.log(searchTerm);
+            console.log(results);
+            // console.log(results[0].result[0]);
+            // console.log(results[0].result[1]);
+            // console.log(results[0].result[2]);
+            // searchTerm = results[0].result;
+            // list.innerText = searchTerm;
           })
           .catch(function (error) {
             heading.innerText =
@@ -40,12 +50,26 @@ function onWindowLoad() {
   );
 }
 
-function DOMtoString(selector) {
-  if (selector) {
+function DOMtoString(selector, selector1, selector2, selector3) {
+  if (selector || selector1 || selector2 || selector3) {
     selector = document.querySelector(selector);
-    if (!selector) return "ERROR: querySelector failed to find node";
+    selector1 = document.querySelector(selector1);
+    selector2 = document.querySelectorAll(selector2);
+    selector3 = document.querySelectorAll(selector3);
+    if (!selector && !selector1 && !selector2 && !selector3)
+      return "No node found";
   } else {
     selector = document.documentElement;
+    selector1 = document.documentElement;
+    selector2 = document.documentElement;
+    selector3 = document.documentElement;
   }
-  return selector.outerHTML;
+
+  let h2Terms = [];
+  let h3Terms = [];
+
+  selector2.forEach((item) => h2Terms.push(item.innerText));
+  selector3.forEach((item) => h3Terms.push(item.innerText));
+
+  return [selector.innerText, selector1.innerText, h2Terms, h3Terms];
 }
