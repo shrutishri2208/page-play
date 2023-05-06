c93b680eb00b65b69ab199ec180d554a5af73319
Shruti Shrivastava <shrutishri2208@gmail.com>
2023-05-06 20:07:54 +0530

    channelID

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,181 @@
+const object = [
+  {
+    kind: "youtube#searchResult",
+    etag: "icdlqaYyfDiV_JFP8YYAV1VPnYQ",
+    id: {
+      kind: "youtube#video",
+      videoId: "34Na4j8AVgA",
+    },
+    snippet: {
+      publishedAt: "2016-09-28T16:00:01Z",
+      channelId: "UCF_fDSgPpBQuh1MsUTgIARQ",
+      title: "The Weeknd - Starboy ft. Daft Punk (Official Video)",
+      description:
+        "Starboy ft. Daft Punk (Official Video) Taken from the new album Starboy http://theweeknd.co/StarboyYD Connect with The ...",
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/34Na4j8AVgA/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/34Na4j8AVgA/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/34Na4j8AVgA/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: "TheWeekndVEVO",
+      liveBroadcastContent: "none",
+      publishTime: "2016-09-28T16:00:01Z",
+    },
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: "BuiX1OAQk6VhjWEOLDcOSd3wAao",
+    id: {
+      kind: "youtube#video",
+      videoId: "dMoFcvfd5t4",
+    },
+    snippet: {
+      publishedAt: "2023-04-21T04:00:15Z",
+      channelId: "UCF_fDSgPpBQuh1MsUTgIARQ",
+      title: "The Weeknd ft. Future - Double Fantasy (Official Music Video)",
+      description:
+        "Music video by The Weeknd performing Double Fantasy. © 2023 The Weeknd XO, Inc., manufactured and marketed by Republic ...",
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/dMoFcvfd5t4/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/dMoFcvfd5t4/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/dMoFcvfd5t4/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: "TheWeekndVEVO",
+      liveBroadcastContent: "none",
+      publishTime: "2023-04-21T04:00:15Z",
+    },
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: "BPuAmCV-k7uGoYu5_3EvqtJS2cU",
+    id: {
+      kind: "youtube#video",
+      videoId: "JZjAg6fK-BQ",
+    },
+    snippet: {
+      publishedAt: "2017-02-16T17:00:03Z",
+      channelId: "UCF_fDSgPpBQuh1MsUTgIARQ",
+      title: "The Weeknd - Reminder (Official Video)",
+      description:
+        "Reminder (Official Video) Taken from the album Starboy http://theweeknd.co/StarboyYD XO Official Store: ...",
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/JZjAg6fK-BQ/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/JZjAg6fK-BQ/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/JZjAg6fK-BQ/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: "TheWeekndVEVO",
+      liveBroadcastContent: "none",
+      publishTime: "2017-02-16T17:00:03Z",
+    },
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: "Q5l1dvGN0JIqFPI4Iv5imgu8A40",
+    id: {
+      kind: "youtube#video",
+      videoId: "4NRXx6U8ABQ",
+    },
+    snippet: {
+      publishedAt: "2020-01-21T18:00:10Z",
+      channelId: "UCF_fDSgPpBQuh1MsUTgIARQ",
+      title: "The Weeknd - Blinding Lights (Official Video)",
+      description:
+        'Official music video for The Weeknd "Blinding Lights" - available everywhere now: http://theweeknd.co/blindinglightsYD ...',
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/4NRXx6U8ABQ/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/4NRXx6U8ABQ/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/4NRXx6U8ABQ/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: "TheWeekndVEVO",
+      liveBroadcastContent: "none",
+      publishTime: "2020-01-21T18:00:10Z",
+    },
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: "tZTDiPVH2hI8oauxttn4gs6mYsA",
+    id: {
+      kind: "youtube#video",
+      videoId: "XXYlFuWEuKI",
+    },
+    snippet: {
+      publishedAt: "2021-01-05T17:00:12Z",
+      channelId: "UCF_fDSgPpBQuh1MsUTgIARQ",
+      title: "The Weeknd - Save Your Tears (Official Music Video)",
+      description:
+        "Official music video by The Weeknd performing \"Save Your Tears\"– 'After Hours' available everywhere now: ...",
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/XXYlFuWEuKI/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/XXYlFuWEuKI/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/XXYlFuWEuKI/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: "TheWeekndVEVO",
+      liveBroadcastContent: "none",
+      publishTime: "2021-01-05T17:00:12Z",
+    },
+  },
+];
+
 window.onload = onWindowLoad;
 
 let heading = document.getElementById("heading");
@@ -5,6 +183,9 @@ let list = document.getElementById("list");
 list.innerHTML = `<h2>Loading...</h2>`;
 
 const fetchData = async (searchTerm) => {
+  // const response = await fetch(
+  //   `https://www.googleapis.com/youtube/v3/search?part=snippet&q=the%20weeknd&type=video&key=${YOUTUBE_DATA_API}`
+  // );
   const response = await fetch(
     `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchTerm}&type=video&key=${YOUTUBE_DATA_API}`
   );
@@ -63,7 +244,9 @@ const displayList = (displayList) => {
       <img src="${item.snippet.thumbnails.medium.url}" alt="Video Thumbnail" width="320" height="180">
       <h2>${item.snippet.title}</h2>
     </a>
-    <h3>${item.snippet.channelTitle}</h3>
+    <a target='_blank' href="https://www.youtube.com/channel/${item.snippet.channelId}">
+      <h3>${item.snippet.channelTitle}</h3>
+    </a>
   </div>
   `)
   );
